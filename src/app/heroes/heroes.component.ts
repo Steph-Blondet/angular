85d8a56551648a1f28b4df850a747abefdbb6ae4
Stephanie Blondet <stephanieblondet@Stephanies-iMac.fios-router.home>
2018-05-21 08:26:29 -0400

    01 Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 // Import the Hero model
 import { Hero } from '../hero';
+// Import the mock HEROES
+import { HEROES } from '../mock-heroes'
+
 
 // Decorator function that specifies the Angular metadata for the component.
 @Component({
@@ -17,15 +20,26 @@ export class HeroesComponent implements OnInit {
   // hero = "Windstorm";
 
   // Refactor the component's hero property to be of type Hero. Initialize it with an id of 1 and the name Windstorm.
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  // Add a heroes property to the class that exposes these heroes for binding
+  heroes = HEROES;
+
+  // Rename the component's hero property to selectedHero but don't assign it. There is no selected hero when the application starts.
+  selectedHero: Hero;
 
   constructor() { }
 
   // Lifecycle hook Angular calls ngOnInit shortly after creating a component. It's a good place to put initialization logic.
   ngOnInit() {
+  }
+
+  // Add the following onSelect() method, which assigns the clicked hero from the template to the component's selectedHero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
